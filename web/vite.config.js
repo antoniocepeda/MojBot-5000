dac55b9653f4bc62a4fdcdbeb3924cbeeaa66808
Antonio Cepeda <antoniocepeda_lvl3@icloud.com>
2026-03-22 00:38:55 -0500

    fix: set base path in vite config to fix asset loading

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: './', // This ensures all generated asset paths are relative
   build: {
     rollupOptions: {
       input: {
